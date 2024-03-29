0bfca481ed2ac1e32bb3edbb86c184923ebfbf07
sirx2713 <bradx2713@gmail.com>
2024-03-29 13:07:39 +0200

    Initial Commit

---
+++ Hike_D2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Hike_D2
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike_D2/Hike_D2App.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_D2App.swift
+//  Hike_D2
+//
+//  Created by Tafadzwa Alexander Razaro on 2024/03/29.
+//
+
+import SwiftUI
+
+@main
+struct Hike_D2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
